0eff3eb132dfff622d510377d70bb25d82b315c0
AJ Schmidt <ajs4392@gmail.com>
2019-06-23 22:51:52 -0400

    add margins

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,16 +3,26 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Table from '../Table/Table';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default function AppComponent() {
+  const classes = makeStyles({
+    container: {
+      margin: '48px auto'
+    },
+    content: {
+      marginBottom: '32px'
+    }
+  })();
+
   return (
     <>
       <CssBaseline />
-      <Container maxWidth="lg">
+      <Container className={classes.container} maxWidth="lg">
         <Typography component="h1" variant="h3" gutterBottom>
           Hospital IQ
         </Typography>
-        <Typography component="p" paragraph>
+        <Typography className={classes.content} component="p" paragraph>
           View each unit's occupancy information in the table below. Units
           highlighted in yellow require attention.
         </Typography>
